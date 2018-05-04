298b91c4b115ff212576edc4ef902ac0d40229ff
joeos014 <joeos014@student.liu.se>
2018-05-04 23:43:25 +0200

    Better input. Playercontroller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public string[] inputNames;
+
+	// Use this for initialization
+	void Start () {
+        	
+	}
+	
+    void selectCard(int i)
+    {
+        Debug.Log("Selcted card");
+        Debug.Log(i);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        for(int i = 0; i < 4; ++i)
+        {
+            if (Input.GetButton(inputNames[i]))
+            {
+                this.selectCard(i);
+            }
+        }
+    }
+}
