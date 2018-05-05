f5d3ee0467382d6a14ba52f1f88d365d125a97db
Jojoje <joani718@student.liu.se>
2018-05-05 10:18:13 +0200

    added gamemaster script

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+    public PlayerController[] players = new PlayerController[1];
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
