e7363660d2bf9a22705a664ab8a00743c0b5ea37
joeos014 <joeos014@student.liu.se>
2018-05-06 14:53:57 +0200

    Make hearts fall

--- Assets/Scripts/HealthIndicatorController.cs
+++ Assets/Scripts/HealthIndicatorController.cs
@@ -5,20 +5,51 @@ using UnityEngine;
 public class HealthIndicatorController : MonoBehaviour {
 
     public PlayerController player;
-    public SpriteRenderer[] hearts = new SpriteRenderer[5];
+    public GameObject[] hearts = new GameObject[5];
+
+    private bool[] heartsAlive;
+    private ArrayList moveHearts = new ArrayList();
+
+    public float MOVE_SPEED = 10;
 
     // Use this for initialization
     void Start () {
-		
+        this.heartsAlive = new bool[hearts.Length];
+
+        for (int i = 0; i < hearts.Length; ++i)
+        {
+            this.heartsAlive[i] = true;
+        }
 	}
+
+    private void killHeart(int i)
+    {
+        this.moveHearts.Add(this.hearts[i]);
+        this.heartsAlive[i] = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        foreach(var heart in this.moveHearts)
+        {
+            if (((GameObject)heart).transform.position.y >= -20)
+            {
+                ((GameObject)heart).transform.Translate(Vector3.down * Time.deltaTime * this.MOVE_SPEED);
+            }
+        }
+
         // Make remaning hearts visible
         for (int hp_i = 0; hp_i < hearts.Length; ++hp_i)
         {
-            hearts[hp_i].enabled = player.health > hp_i;
+            if (
+                !(player.health > hp_i)
+                &&
+                heartsAlive[hp_i]
+                )
+            {
+                killHeart(hp_i);
+            }
         }
     }
 }
